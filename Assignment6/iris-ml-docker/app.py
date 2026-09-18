e04f2d21351db81b831089c320a497886387040e
Raghs3 <tinku.minku2803@gmail.com>
2026-09-18 15:55:27 +0530

    Add Flask REST API app.py for Iris model serving

---
+++ Assignment6/iris-ml-docker/app.py
@@ -0,0 +1,21 @@
+# app.py
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+model = joblib.load('iris_model.pkl')
+
+@app.route('/', methods=['GET'])
+def home():
+    return jsonify({'message': 'Iris Model API is running'})
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    features = np.array(data['features']).reshape(1, -1)
+    prediction = model.predict(features)
+    return jsonify({'prediction': int(prediction[0])})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
